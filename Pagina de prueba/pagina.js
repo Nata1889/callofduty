be20a43185ef6ab8ef841dcb19a7fb89be5edbe5
Nata1889 <107150630+Nata1889@users.noreply.github.com>
2023-07-31 20:19:04 -0300

    Add files via upload

---
+++ Pagina de prueba/pagina.js
@@ -0,0 +1,19 @@
+function redirectTo() {
+    window.location.href = 'https://www.callofduty.com/es/playnow/modernwarfare2?standalone=1';
+}
+
+function redirectTo2() {
+    window.location.href = 'https://www.callofduty.com/es/playnow/warzone?standalone=1';
+}
+
+function redirectTo3() {
+    window.location.href = 'https://www.callofduty.com/es/playnow/vanguard?standalone=1';
+}
+
+function redirectTo4() {
+    window.location.href = 'https://store.steampowered.com/app/10180/Call_of_Duty_Modern_Warfare_2_2009/';
+}
+
+function redirectTo5() {
+    window.location.href = 'https://store.steampowered.com/app/42700/Call_of_Duty_Black_Ops/';
+}
